621d9cb2a8a2958b1fab53b07e0e8adff453e301
Claas Ahlrichs <claasahl@gmail.com>
2021-03-24 18:55:34 +0100

    updated scripts

--- inflate.ts
+++ inflate.ts
@@ -1,7 +1,12 @@
 import rimraf from "rimraf";
 import AdmZip from "adm-zip";
 
-const project = "holy-dowels";
+if(process.argv.length < 3) {
+    console.log("\n\nnpm run inflate {name-of-project}\n\n");
+    process.exit(1);
+}
+
+const project = process.argv[2];
 const zipFile = `./${project}.FCStd`
 const folder = `./${project}/`
 
